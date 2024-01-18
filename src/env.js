f446a93120e7a43af091945ae0c5a8c7685a676e
lucas-felinto <lucasfepinto@gmail.com>
2024-01-18 14:52:05 -0300

    chore: add maps api package

--- src/env.js
+++ src/env.js
@@ -17,6 +17,7 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    GOOGLE_MAPS_API_KEY: z.string()
   },
 
   /**
@@ -35,6 +36,7 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
