05a4e30299a63857e5114c7180b48669267e20e2
lucas-felinto <lucasfepinto@gmail.com>
2024-01-23 11:31:56 -0300

    feat: add driver and rider hooks

---
+++ src/app/hooks/UseDriver.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect, useCallback } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { Person } from '../components/UserCard';
+
+const useDriver = () => {
+  const [selectedDriver, setSelectedDriver] = useState<Person>();
+
+  const fetchDrivers = useCallback(
+    () => fetch('api/drivers').then((res) => res.json()),
+    []
+  );
+
+  const { data: drivers } = useQuery({
+    queryKey: ['riders'],
+    queryFn: fetchDrivers,
+  });
+
+  useEffect(() => {
+    if (drivers && drivers.length > 0 && !selectedDriver) {
+      setSelectedDriver(drivers[0]);
+    }
+  }, [drivers, selectedDriver]);
+
+  return { drivers, selectedDriver, setSelectedDriver };
+};
+
+export default useDriver;
---
+++ src/app/hooks/UseRider.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect, useCallback } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { Person } from '../components/UserCard';
+
+const useRider = () => {
+  const [selectedRider, setSelectedRider] = useState<Person>();
+
+  const fetchRiders = useCallback(
+    () => fetch('api/riders').then((res) => res.json()),
+    []
+  );
+
+  const { data: riders } = useQuery({
+    queryKey: ['riders'],
+    queryFn: fetchRiders,
+  });
+
+  useEffect(() => {
+    if (riders && riders.length > 0 && !selectedRider) {
+      setSelectedRider(riders[0]);
+    }
+  }, [riders, selectedRider]);
+
+  return { riders, selectedRider, setSelectedRider };
+};
+
+export default useRider;
